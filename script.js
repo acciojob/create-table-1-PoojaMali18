3f1e11efc2593a75ba0aee4413d9286515c1df47
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-10-24 11:36:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+let v = document.createElement("TR");
+	let  b=document.createElement("td");
+	b.innerText("New Cell1");
+	let  f=document.createElement("td");
+	f.innerText("New Cell2");
+	v.appendChild(b);
+	b.appendChild(f);
+tr.appendChild(v);
 }
