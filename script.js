edfe03329b82e28ece162c6bfade48e4e9bec82c
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-10-24 11:44:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 function insert_Row() {
-let v = document.createElement("TR");
-	let  b=document.createElement("td");
-	b.innerText("New Cell1");
-	let  f=document.createElement("td");
-	f.innerText("New Cell2");
-	v.appendChild(b);
-	v.appendChild(f);
- let table = document.getElementById("sampleTable");
+    // Create a new row
+    let newRow = document.createElement("tr");
 
+    // Create the left cell with the text "New Cell1"
+    let cell1 = document.createElement("td");
+    cell1.textContent = "New Cell1";
 
+    // Create the right cell with the text "New Cell2"
+    let cell2 = document.createElement("td");
+    cell2.textContent = "New Cell2";
+
+    // Append the cells to the new row
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+
+    // Get a reference to the table with id "sampleTable"
+    let table = document.getElementById("sampleTable");
+
+    // Insert the new row at the top of the table
     if (table) {
-        table.insertBefore(v, table.firstChild);
+        table.insertBefore(newRow, table.firstChild);
     }
 }
+
